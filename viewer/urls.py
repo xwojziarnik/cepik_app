136d82ea649156abe3a15a00ab5d8cd03af49ca5
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-28 14:48:11 +0200

    Created templates

--- viewer/urls.py
+++ viewer/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.home),
+    path('vehicles', views.vehicles),
+    path('driving-licenses', views.driving_licenses),
+    path('login', views.login),
+    path('register/', views.register),
+    path('about', views.about),
 ]
