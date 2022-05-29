6ed9fea26bae16ddc301179084f0eb099141bb0f
patryk_s <boydmalvo@gmail.com>
2022-05-29 15:36:55 +0200

    add comments to utils, and objects to local db (still not enough...)

--- viewer/utils.py
+++ viewer/utils.py
@@ -1,41 +1,70 @@
-from django.shortcuts import render
+"""
+Hi! You are in back-end side of 'cepik_app. So, if you are a 'Front-end Team' - run away! ;)
+There are three funcs. 'Download_vehicles', 'Download_licences', and 'download_data' to
+automatic both. Running is shell.
+Voivodeships:
+02 dolnośląskie
+04 kujawsko-pomorskie
+06 lubelskie
+08 lubuskie
+10 łódzkie
+12 małopolskie
+14 mazowieckie
+16 opolskie
+18 podkarpackie
+20 podlaskie
+22 pomorskie
+24 śląskie
+26 świętokrzyskie
+28 warm-mazurskie
+30 wielkpolskie
+32 zachodniopomorskie
+"""
+
+import time
 import requests
 from viewer.models import Vehicle, Driving_licenses
-import time
 
 
-def all_voivodeships():
-    voivo = ['08', '10', '12', '14', '16''18', '20', '22',
-             '24', '26', '28', '30', '32']
-    vdl = input('type 1(vehicles) or 2(driving_licenses): ')
-    if vdl == '1':
-        for num in voivo:
-            for page in range(1, 11):
-                if page % 2 == 0:
-                    print('hold')
+def download_data():
+    """
+    download data from API for all voivodeships.
+    """
+    voivodeships = ['02', '04', '06', '08', '10', '12', '14', '16', '18', '20', '22',
+                    '24', '26', '28', '30', '32']  # list of voivodeships. Explanation in docstring.
+    choose = input('type 1(vehicles) or 2(driving_licenses): ')
+    if choose == '1':
+        for voivo in voivodeships:
+            for page in range(1, 11):   # API limits - 100 calling/ one minute
+                if page % 2 == 0:       # 100 result per page
+                    print('hold')       # next page needs to wait 60 seconds
                     time.sleep(60)
                 try:
-                    url = f'https://api.cepik.gov.pl/pojazdy?wojewodztwo={num}&data-od=20220501&page={page}'
-                    print(f'voivodeship {num}')
+                    url = f'https://api.cepik.gov.pl/pojazdy?wojewodztwo={voivo}&data-od=20220403&page={page}'
+                    print(f'voivodeship {voivo}')
                     print(f'page {page}')
-                    api_to_db(url)
+                    download_vehicles(url)
                 except IndexError:
                     break
 
-    elif vdl == '2':
-        for num in voivo:
+    elif choose == '2':
+        for voivo in voivodeships:
             for page in range(1, 11):
                 try:
-                    url = f'https://api.cepik.gov.pl/prawa-jazdy?filter[wojewodztwo-kod]={num}&page={page}'
-                    print(num)
-                    api_to_db_licences(url)
+                    url = f'https://api.cepik.gov.pl/prawa-jazdy?filter[wojewodztwo-kod]={voivo}&page={page}'
+                    print(voivo)
+                    download_licences(url)
                 except IndexError:
                     break
     else:
         print('wrong input')
 
 
-def api_to_db(url):
+def download_vehicles(url):
+    """
+    Func send request to API and turn response into json.
+    Next transforms selected data from json to local database.
+    """
     requests.packages.urllib3.disable_warnings()  # type:ignore
     requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'  # type:ignore
     response = requests.get(url)
@@ -43,16 +72,16 @@ def api_to_db(url):
     if vehicles['data']:
         vehicles = vehicles['data']
         count = 0
-        for i in vehicles:
+        for veh in vehicles:
             requests.packages.urllib3.disable_warnings()  # type:ignore
             requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'  # type:ignore
-            url = i['links']['self']
+            url = veh['links']['self']
             response = requests.get(url)
             vehicle = response.json()
 
             vehicle = vehicle['data']
 
-            vehicle_in_database = Vehicle.objects.filter(id_cepik=vehicle['id']).exists()
+            vehicle_in_database = Vehicle.objects.filter(id_cepik=vehicle['id']).exists()  # type: ignore
             if not vehicle_in_database:
                 vehs = Vehicle(id_cepik=vehicle['id'], id_wojewodztwa=vehicle['attributes']['wojewodztwo-kod'],
                                wojewodztwo=vehicle['attributes']['rejestracja-wojewodztwo'],
@@ -71,13 +100,17 @@ def api_to_db(url):
                 print('done - ', count)
 
 
-def api_to_db_licences(url):
+def download_licences(url):
+    """
+    Func send request to API and turn response into json.
+    Next transforms selected data from json to local database.
+    """
     requests.packages.urllib3.disable_warnings()  # type:ignore
     requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'  # type:ignore
     response = requests.get(url)
     licenses = response.json()
     licenses = licenses['data'][0]
-    licenses_in_database = Driving_licenses.objects.filter(id_cepik=licenses['id']).exists()
+    licenses_in_database = Driving_licenses.objects.filter(id_cepik=licenses['id']).exists()  # type: ignore
     count = 0
     if not licenses_in_database:
         licens = Driving_licenses(id_cepik=licenses['id'],
@@ -91,4 +124,4 @@ def api_to_db_licences(url):
         licens.save()
         count += 1
         print('done - ', count)
-    ...
+
