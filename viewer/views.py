a4aaf0597c42909cc45a92d74629765b91df0d7e
Lkacik <cep6@poczta.fm>
2022-05-31 19:51:45 +0200

    Added def interesting_facts in views

--- viewer/views.py
+++ viewer/views.py
@@ -1,5 +1,8 @@
+import random
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from viewer.models import Vehicle, Driving_licenses
 
 # Create your views here.
 
@@ -14,3 +17,19 @@ def driving_licenses(request):
 
 def about(request):
     return render(request, template_name='about.html')
+
+
+def interesting_facts():
+    petrol_cars = Vehicle.objects.filter(rodzaj_paliwa='BENZYNA').count()
+    rope_cars = Vehicle.objects.filter(rodzaj_paliwa='OLEJ NAPĘDOWY').count()
+    gas_cars = Vehicle.objects.filter(rodzaj_paliwa__contains='GAZ').count()
+    voivodeships = ['DOLNOŚLĄSKIE', 'KUJAWSKO-POMORSKIE', 'LUBELSKIE', 'ŁÓDZKIE', 'MAŁOPOLSKIE', 'MAZOWIECKIE', 'OPOLSKIE', 'PODKARPACKIE', 'PODLASKIE', 'POMORSKIE', 'ŚLĄSKIE', 'ŚWIĘTOKRZYSKIE', 'WARMIŃSKO-MAZURSKIE', 'WIELKOPOLSKIE', 'ZACHODNIOPOMORSKIE']
+    random_voivodeship = random.choice(voivodeships)
+    # for voivodeship in voivodeships:
+    #     result = Vehicle.objects.filter(wojewodztwo=voivodeship).count()
+    #     print(f"Liczba zarejestrowanych pojazdów, województwo {voivodeship.title()}: {result}.")
+    number_all_cars = Vehicle.objects.filter(wojewodztwo=random_voivodeship).count()
+    number_one_mar_cars = Vehicle.objects.filter(wojewodztwo=random_voivodeship, marka='TOYOTA').count()
+    math_result = number_one_mar_cars*100/number_all_cars
+    print(f"{round(math_result,2)}% aut województwa {random_voivodeship.title()} to Toyoty.")
+    print(f"Ilość zarejestrowanych pojazdów na paliwo: {petrol_cars}, rope: {rope_cars}, gaz: {gas_cars}.")
